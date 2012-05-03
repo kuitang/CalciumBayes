e8a02d7a388345cdd58907bc86debb0e842afba9
Kui Tang <kuitang@gmail.com>
2012-05-03 01:56:47 -0400

    fixed output arg in m_step_full

--- src/m_step_full.m
+++ src/m_step_full.m
@@ -1,13 +1,13 @@
-function [ output_args ] = m_step_full(theta_intrinsic, options, N, varargin)
+function [ theta ] = m_step_full(theta, options, N, varargin)
 %M_STEP_FULL Summary of this function goes here
 %   Detailed explanation goes here
 
 
-    lb = ones(size(theta_intrinsic)) * -2;
-    ub = ones(size(theta_intrinsic)) * 2;
+    lb = ones(size(theta)) * -2;
+    ub = ones(size(theta)) * 2;
     lb(1) = -5; lb(2:N+1) = -10;
     ub(1) = 5; ub(2:N+1) = 10;
-    [theta_intrinsic,fval,exitflag,output] = fmincon('q_single_neuron_full',theta_intrinsic, [], [], [], [], lb, ub,[],options, varargin{:});
+    [theta,fval,exitflag,output] = fmincon('q_single_neuron_full',theta, [], [], [], [], lb, ub,[],options, varargin{:});
 
 end
 
