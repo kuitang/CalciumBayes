15d2615eb04294a84d8a00d2dd620a73f6cfe12e
Ben Shababo <benshababo@gmail.com>
2012-05-03 00:50:16 -0400

    main and m_step for full are done to opt over w,b,beta full

--- src/m_step_full.m
+++ src/m_step_full.m
@@ -1,12 +1,12 @@
-function [ output_args ] = m_step_full(theta_intrinsic, oprivarargin)
+function [ output_args ] = m_step_full(theta_intrinsic, options, N, varargin)
 %M_STEP_FULL Summary of this function goes here
 %   Detailed explanation goes here
 
 
     lb = ones(size(theta_intrinsic)) * -2;
     ub = ones(size(theta_intrinsic)) * 2;
-    lb(1) = -5; lb(2) = -10;
-    ub(1) = 5; ub(2) = 10;
+    lb(1) = -5; lb(2:N+1) = -10;
+    ub(1) = 5; ub(2:N+1) = 10;
     [theta_intrinsic,fval,exitflag,output] = fmincon('q_single_neuron',theta_intrinsic, [], [], [], [], lb, ub,[],options, varargin{:});
 
 end
--- src/main.m
+++ src/main.m
@@ -42,6 +42,7 @@ spmd(N)
     % lambda = ones(N, S);
     b = zeros(N, 1, codist);
     w = ones(N, N, codist) .* .1*exprnd(.9,N);
+    p_weights = zeros(N,T,M,codist);
     % for i=1:N/5
     %     rand_inh
     %     w(i,:) = -exprnd(2.3,N,1);
@@ -67,23 +68,23 @@ for i = 1:N
     
 end
 
+
 %% Main loop
 %until the change in connectivity matrix w is below threshold change
-% while(norm(w - w_prev) > thresh_w)
-    
 
+w_prev = ones(size(w)) * 500;
+thresh_w = .001;
+
+while(norm(w - w_prev) > thresh_w)
+    
+    spmd(N)   
     %parfor i = 1 : N
     ll = -Inf;
-    spmd(N)        
+     
         for i = drange(1:N)
 
             disp(['Neuron ' num2str(i) '/' num2str(N)]);            
 
-            
-            iter = 1;
-
-            %theta_intrinsic = [b_i w_ii beta_ii(2:S)' lambda(1:S-1)] % NO
-            %LAMBDA
             %% Initialize the intrinsic parameters
 
             theta_intrinsic = [b(i) w(i,i) reshape(beta(i, i, :),1,S-1)];
@@ -95,10 +96,10 @@ end
                 iter = iter + 1;
                 old_theta_intr = theta_intrinsic;
                 beta_subset = reshape(beta(i,:,:), N, S - 1);
-                [p_weights h(i,:,:,:)] = e_step_smc(i, M, tau, delta, sigma, beta_subset, b(i), w(i,:), n);
+                [p_weights(i,:,:) h(i,:,:,:)] = e_step_smc(i, M, tau, delta, sigma, beta_subset, b(i), w(i,:), n);
 
                 %% M step for the intrinsic parameters for one neuron
-                theta_intrinsic = m_step_smc(theta_intrinsic, optim_options, beta_subset, w(i,:), squeeze(h(i,:,:,:)), n, i, delta, tau, sigma, p_weights);                
+                theta_intrinsic = m_step_smc(theta_intrinsic, optim_options, beta_subset, w(i,:), squeeze(h(i,:,:,:)), n, i, delta, tau, sigma, p_weights(i,:,:));                
                 b(i,1) = theta_intrinsic(1);
                 w(i,i) = theta_intrinsic(2);
                 beta(i, i, :) = reshape(theta_intrinsic(3:1+S),1,1,S-1);
@@ -107,39 +108,32 @@ end
                 disp('abs diff of params');
                 disp(abs(theta_intrinsic - old_theta_intr));
             end
-            
-            %% M step for all parameters
-            
-            %% Compute the new log-likelihood
-    %         save('new_m_step_complete.mat');
+
         end
-    end
+    
+        for i = drange(1:N)
+
+            theta = [b(i) w(i,:) reshape(beta(i, :, :),1,N*S-N)];
+            old_theta_intr = ones(size(theta)) * 500;
+
+            theta = m_step_full(theta, optim_options, N, beta_subset, w(i,:), squeeze(h(i,:,:,:)), n, i, delta, tau, sigma, p_weights(i,:,:));
+
+            b(i,1) = theta_intrinsic(1);
+            w(i,:) = reshape(theta_intrinsic(2:N+1),1,N);
+            beta(i,:) = reshape(theta_intrinsic(N+2:(N*S+3)), 1, N*(S - 1));
+
+        end
+    
     
     nll = log_likelihood(beta, b, w, h, n, delta, ones(T,M));
     %nll = log_likelihood(gather(beta), gather(b), gather(w), gather(h), n, delta, ones(T,M));
     ll = [ll nll];
     disp('ll =');
     disp(ll);
-%         disp('******');
-%         disp(theta_intrinsic);
-%         disp(old_theta_intr);
-%         disp(theta_intrinsic - old_theta_intr);
-%         disp(abs(theta_intrinsic - old_theta_intr));
-%         disp(sum(abs(theta_intrinsic - old_theta_intr)));
+        
+    end
+end
 
-%         while(sum(abs(theta_intrinsic - old_theta_intr)) > .01) %we shoudl make this bigger...
-        %... maybe this: abs(theta_intrinsic - old_theta_intr) > .01
-        %... that is, don't sum, just test each one
-
-
-
-    
-    %% blah blah blah
-    save('finished_run.mat');
-    
-    
-%     m_step_joint(b, w, beta, lambda, tau, sigma, h, truncdata, i, delta, M);
-    
-
+save('finished_run.mat');
 
 
