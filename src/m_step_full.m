f3ad367a2cb06b70d02eaff7382e68af105cc62c
Ben Shababo <benshababo@gmail.com>
2012-05-03 00:26:28 -0400

    blargh

--- src/m_step_full.m
+++ src/m_step_full.m
@@ -1,6 +1,13 @@
-function [ theta ] = m_step_full(theta, options, varargin)
+function [ output_args ] = m_step_full(theta_intrinsic, oprivarargin)
+%M_STEP_FULL Summary of this function goes here
+%   Detailed explanation goes here
 
 
+    lb = ones(size(theta_intrinsic)) * -2;
+    ub = ones(size(theta_intrinsic)) * 2;
+    lb(1) = -5; lb(2) = -10;
+    ub(1) = 5; ub(2) = 10;
+    [theta_intrinsic,fval,exitflag,output] = fmincon('q_single_neuron',theta_intrinsic, [], [], [], [], lb, ub,[],options, varargin{:});
 
 end
 
