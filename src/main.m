befb625b7780c4123f29e51425c412a51d74afc0
Kui Tang <kuitang@gmail.com>
2012-05-02 23:10:01 -0400

    Parallelization works

--- src/main.m
+++ src/main.m
@@ -29,23 +29,28 @@ M = 50; % size of particle sampler
 
 % Parameter matrices
 % TODO: Set up priors!
-beta = zeros(N, N, S-1);
-% lambda = ones(N, S);
-b = zeros(1, N);
-w = .1*exprnd(.9,N);
-% for i=1:N/5
-%     rand_inh
-%     w(i,:) = -exprnd(2.3,N,1);
-% end
 
-w = w .* binornd(1,.1,N,N);%second arg is "sparesness"
-for i = 1:N   
-    w(i,i) = -abs(normrnd(.6,.2));
+%% Set codistributed arrays
+
+spmd(N)
+    codist = codistributor1d(1);
+    beta = zeros(N, N, S-1, codist);
+    % lambda = ones(N, S);
+    b = zeros(N, 1, codist);
+    w = ones(N, N, codist) .* .1*exprnd(.9,N);
+    % for i=1:N/5
+    %     rand_inh
+    %     w(i,:) = -exprnd(2.3,N,1);
+    % end
+
+    w = w .* binornd(1,.1,N,N);%second arg is "sparesness"
+
+    h = zeros(N,N,T,M, codist);
+
+    for i = drange(1:N)
+        w(i,i) = -abs(normrnd(.6,.2));
+    end
 end
-
-theta_intrinsic_thresh = .01; %???
-
-h = zeros(N,N,T,M);
 
 % load('first_e_step_complete.mat');
 % first = 1;
@@ -55,74 +60,68 @@ h = zeros(N,N,T,M);
 % while(sum(sum(abs(w - w_prev))) > thresh_w)
     
     %parfor i = 1 : N
-    for i = 1:N
-                
-        disp(['Neuron ' num2str(i) '/' num2str(N)]);
+    spmd(N)        
+        for i = drange(1:N)
 
-        ll = -Inf;
-        iter = 1;
-        
-        %theta_intrinsic = [b_i w_ii beta_ii(2:S)' lambda(1:S-1)] % NO
-        %LAMBDA
-        %% Initialize the intrinsic parameters
-        theta_intrinsic = [b(i) w(i,i) reshape(beta(i, i, :),1,S-1)];
-        old_theta_intr = ones(size(theta_intrinsic)) * 500;
-        
-        %% Let the intrinsic parameters converge
-        while( any(abs(theta_intrinsic - old_theta_intr) > 1e-4))
-            %% E step (SMC) for one neuron
-            iter = iter + 1;
-            old_theta_intr = theta_intrinsic;
-            beta_subset = reshape(beta(i,:,:), N, S - 1);
-            [p_weights h(i,:,:,:)] = e_step_smc(i, M, tau, delta, sigma, beta_subset, b(i), w(i,:), n);
-            
-            %% M step for the intrinsic parameters for one neuron
-            theta_intrinsic = m_step_smc(theta_intrinsic, beta_subset, w(i,:), squeeze(h(i,:,:,:)), n, i, delta, tau, sigma, p_weights);
-            b(i) = theta_intrinsic(1);
-            w(i,i) = theta_intrinsic(2);
-            beta(i, i, :) = reshape(theta_intrinsic(3:1+S),1,1,S-1);
-            disp('new params:');
-            disp(theta_intrinsic);
-            disp('abs diff of params');
-            disp(abs(theta_intrinsic - old_theta_intr));
-%             lambda(i,:) = theta_intrinsic(3+S:2*S+2);
+            disp(['Neuron ' num2str(i) '/' num2str(N)]);            
 
-%             nll = log_likelihood(params, h, n, delta, p_weights);
-%             ll = [ll nll];
-%             disp('ll =');
-%             disp(ll);
-            
-        end
+            ll = -Inf;
+            iter = 1;
+
+            %theta_intrinsic = [b_i w_ii beta_ii(2:S)' lambda(1:S-1)] % NO
+            %LAMBDA
+            %% Initialize the intrinsic parameters
+
+            theta_intrinsic = [b(i) w(i,i) reshape(beta(i, i, :),1,S-1)];
+            old_theta_intr = ones(size(theta_intrinsic)) * 500;
+
+            %% Let the intrinsic parameters converge
+            while( any(abs(theta_intrinsic - old_theta_intr) > 1e-4))
+                %% E step (SMC) for one neuron
+                iter = iter + 1;
+                old_theta_intr = theta_intrinsic;
+                beta_subset = reshape(beta(i,:,:), N, S - 1);
+                [p_weights h(i,:,:,:)] = e_step_smc(i, M, tau, delta, sigma, beta_subset, b(i), w(i,:), n);
+
+                %% M step for the intrinsic parameters for one neuron
+                theta_intrinsic = m_step_smc(theta_intrinsic, beta_subset, w(i,:), squeeze(h(i,:,:,:)), n, i, delta, tau, sigma, p_weights);                
+                b(i,1) = theta_intrinsic(1);
+                w(i,i) = theta_intrinsic(2);
+                beta(i, i, :) = reshape(theta_intrinsic(3:1+S),1,1,S-1);
+                disp('new params:');
+                disp(theta_intrinsic);
+                disp('abs diff of params');
+                disp(abs(theta_intrinsic - old_theta_intr));
+            end
+
+
+            %% Compute the new log-likelihood        
+    %         save('new_m_step_complete.mat');
+
+            nll = log_likelihood(beta, b, w, h, n, delta, p_weights);
+            ll = [ll nll];
+            disp('ll =');
+            disp(ll);
+    %         disp('******');
+    %         disp(theta_intrinsic);
+    %         disp(old_theta_intr);
+    %         disp(theta_intrinsic - old_theta_intr);
+    %         disp(abs(theta_intrinsic - old_theta_intr));
+    %         disp(sum(abs(theta_intrinsic - old_theta_intr)));
+
+    %         while(sum(abs(theta_intrinsic - old_theta_intr)) > .01) %we shoudl make this bigger...
+                %... maybe this: abs(theta_intrinsic - old_theta_intr) > .01
+                %... that is, don't sum, just test each one
 
         
-        %% Compute the new log-likelihood        
-%         save('new_m_step_complete.mat');
-                
-        nll = log_likelihood(beta, b, w, h, n, delta, p_weights);
-        ll = [ll nll];
-        disp('ll =');
-        disp(ll);
-%         disp('******');
-%         disp(theta_intrinsic);
-%         disp(old_theta_intr);
-%         disp(theta_intrinsic - old_theta_intr);
-%         disp(abs(theta_intrinsic - old_theta_intr));
-%         disp(sum(abs(theta_intrinsic - old_theta_intr)));
-        
-%         while(sum(abs(theta_intrinsic - old_theta_intr)) > .01) %we shoudl make this bigger...
-            %... maybe this: abs(theta_intrinsic - old_theta_intr) > .01
-            %... that is, don't sum, just test each one
-                  
-
-    end
+     end
     
     %% blah blah blah
-    save('finished_run.mat');
+%    save('finished_run.mat');
     
     
 %     m_step_joint(b, w, beta, lambda, tau, sigma, h, truncdata, i, delta, M);
     
-% end
-
+ end
 
 
